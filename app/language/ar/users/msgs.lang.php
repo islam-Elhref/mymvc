ab8f03bb7684e44f5db261e80b88a83323d5d737
Islam <ealhref@yahoo.com>
2021-06-01 22:30:19 +0200

    add msgs.lang file for users

---
+++ app/language/ar/users/msgs.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$_['msg_success_add'] = 'تم اضافة المستخدم name بنجاح';
+$_['msg_success_edit'] = 'تم تعديل المستخدم من name الي other';
+$_['msg_success_delete'] = 'تم حذف المستخدم name بنجاح';
+$_['msg_error_add'] = 'اسم المستخدم موجود مسبقا ؟ لم يتم الحفظ';
+
+
+
+
